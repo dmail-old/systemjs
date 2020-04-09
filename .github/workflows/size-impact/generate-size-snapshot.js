65c20292686da502e6af83f8c705af43ab93e062
Damien Maillard <damien.maillard@dailymotion.com>
2020-04-09 17:46:28 +0200

    Remove debug logs

--- .github/workflows/size-impact/generate-size-snapshot.js
+++ .github/workflows/size-impact/generate-size-snapshot.js
@@ -6,7 +6,6 @@ import {
 } from "@jsenv/github-pull-request-filesize-impact";
 
 generateSnapshotFile({
-  logLevel: "debug",
   projectDirectoryUrl: new URL("../../../", import.meta.url),
   snapshotFileRelativeUrl: process.argv[2],
   trackingConfig: {
